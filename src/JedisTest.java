336f8a6f203e06050249dda1ac333869b9b5ab1e
350258655@qq.com <350258655@qq.com>
2018-02-24 16:07:53 +0800

    更新

--- src/JedisTest.java
+++ src/JedisTest.java
@@ -7,14 +7,6 @@ public class JedisTest {
 	@Test
 	public void test() {
 		
-//		// 1、获得连接对象
-//		Jedis jedis = new Jedis("192.168.11.176", 6379);
-//		
-//		// 2、设置数据
-//		jedis.set("name", "jay");
-//		
-//		// 3、获取数据
-//		System.out.println(jedis.get("name"))
 		
 		// 1、获得连接对象
 		Jedis jedis = JedisPoolUtils.getJedis();;
